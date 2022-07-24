417053d2d3817a3a819dbd319a13f76f76e852dd
w4ngzhen <w4ngzhen@hotmail.com>
2022-07-24 10:35:43 +0800

    setup project drawPixels.

--- drawPixels/main.cpp
+++ drawPixels/main.cpp
@@ -1,4 +1,11 @@
-#include <iostream>
-int main() {
-    std::cout << "hello, drawPixels" << std::endl;
+#include "GameLib/Framework.h"
+
+namespace GameLib{
+    void Framework::update(){
+        unsigned* vram = videoMemory();
+        static unsigned i;
+        vram[ i ] += i * 100;
+        i += 9973; //小于1万的最大的质数
+        i %= ( width() * height() );
+    }
 }
